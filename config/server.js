4b778f2541e351972fea213cd2a6ddb421bdfbe8
Antbarras <antbarras@gmail.com>
2023-08-08 21:48:08 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '535673d36554324c0ebd7f55564d2671'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a6cf40cc-6d3b-4e75-beb5-9316e3c68546'
+};
